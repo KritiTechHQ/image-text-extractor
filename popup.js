8e575f7bba131d47ea0d03bf4cda418e4ec84aa1
KritiTechs <14yashas@gmail.com>
2026-07-18 23:08:41 +0530

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.getElementById('actionBtn').addEventListener('click', () => {
+  const outputDiv = document.getElementById('output');
+  outputDiv.innerText = "Scanning page...";
+  console.log("Button clicked inside Kriti Extract!");
+});
